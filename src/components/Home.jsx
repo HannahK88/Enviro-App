5109759f145292370465920862e098a6c7be1d79
Hannah <hannahkate.swift@gmail.com>
2022-04-25 21:28:01 +0100

    Updates to home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,17 +2,25 @@ import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 const Home = () => {
-    const [locationInput, setLocationInput] = useState('')
+    const [locationInput, setLocationInput] = useState()
+    const [location, setLocation] = useState()
+
     const handleChange = (e) => {
         setLocationInput(e.target.value)
     }
+    const handleClick = (e) => {
+        setLocation(e.target.value)
+    }
+    console.log(location)
     console.log(locationInput)
   return (
     <div className='page-wrapper'>
         <div className='input-container'>
             <label htmlFor="location-input">Enter your City</label>
                 <input name='location-input' type="text" onChange={(e) => handleChange(e)}/>
+                <button value={location} onClick={(e) => handleClick(e)}>GO</button>
         </div>
+        <h3>{location}</h3>
         <div className='result-container'>
           <Link to='/fossil-fuels' className='result-card'>Fossil Fuels Burnt</Link>  
           <Link to='/air-pollution' className='result-card'>Air Pollution</Link>  
